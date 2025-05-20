3ca9701b4dddeb8f4b17392d6f108b5782447e6e
Digdgeo <digd.geografo@gmail.com>
2025-05-20 20:09:36 +0200

    Updating the package

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog
+
+All notable changes to the `ndvi2gif` package will be documented in this file.
+
+---
+
+## [0.0.7] - 2025-05-20
+
+### Added
+- New method `get_ndmi()` to compute the Normalized Difference Moisture Index (NDMI).
+- New (old) method `get_gif()` to download a gif for the selected index/bands.
+- Package structure modernized:
+  - Added `setup.cfg` and `pyproject.toml` (PEP 517/518 compliant).
+  - Optional removal of legacy `setup.py`.
+- Updated dependencies:
+  - `geemap` pinned to version `0.29.5`.
+  - `numpy` constrained to `<2.0` for compatibility.
+
+### Fixed
+- Compatibility issues with recent versions of `geemap`, `xarray`, and `numpy`.
+- Resolved import error caused by the removal of `np.unicode_` in NumPy 2.0.
+
+---
+
+## [0.0.6] - 2023-03-10
+
+### Added
+- Initial public release of the `ndvi2gif` package.
+- Generate seasonal composites and extract statistical summaries from several remote sensing index using Google Earth Engine and geemap.
+- Export to animated GIF and GeoTIFF format.
--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-classifiers = [
-	'Intended Audience :: GIS & Remote Sensing Technicians',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Operating System :: OS Independent'
-]
-
-
-setup(
-	name='ndvi2gif',
-	version='0.0.6',
-	description='Python package to create seasonal composites of NDVI or several more vegetation, water, snow, etc. indexes, and download them as pretty gifs or geotiff rasters',
-	long_description=read('README.rst'),
-	url='https://github.com/Digdgeo/Ndvi2Gif',
-	python_requires='>=3.8',
-	author='Diego Garcia Diaz',
-	author_email='digd.geografo@gmail.com',
-	license='MIT',
-	install_requires=['geemap >= 0.19.0', 'deims >= 3.1'],
-	packages=find_packages(include=['ndvi2gif', 'ndvi2gif.*']),
-	zip_safe=False
-)
+setup()
